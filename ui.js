9b0c41a6b4cca2ba59c47868be542ebdddc951a4
779g <adrian.villa@779.mx>
2020-11-08 15:54:20 +0000

    first commit

---
+++ ui.js
@@ -0,0 +1,13 @@
+'use strict'
+const React = require('react')
+const {Text} = require('ink')
+
+const App = ({name = 'Stranger'}) => (
+  <Text>
+		Hello, 
+    {' '}
+    <Text color="green">{name}</Text>
+  </Text>
+)
+
+module.exports = App
